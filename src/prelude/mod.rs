8f696a3d4c3f3bb80371e093403121596fb78452
Pedro Alves <pedromfa98@gmail.com>
2023-05-22 19:55:48 +0100

    defined an overall struct

---
+++ src/prelude/mod.rs
@@ -0,0 +1,22 @@
+/* Contents of prelude module
+
+- user is able to design his own network
+--- choose complex method
+--- build his arch: conventional or sequence based networks
+
+- user is able to train, test and validate his own network
+--- provided a dataset struct (can be pre-preprocessed from an external file or simulated)
+------ tabular dataset (numeric)
+------ image dataset
+------ text dataset
+--- provided a .csv file
+
+- user is able to evaluate and fine tune his own network
+
+- ambition: user is able to obtain explanations for its results
+
+ */
+
+mod neuron;
+mod layer;
+mod network;
