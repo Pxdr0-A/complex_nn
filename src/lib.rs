b1205c5de01bf24c21ce0ae1b8c6edbd882d6915
Pedro Alves <pedromfa98@gmail.com>
2023-05-25 17:58:38 +0100

    turned modules public and tested new methods and functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-mod prelude;
+pub mod prelude;
+pub mod math;
 
 #[cfg(test)]
 mod tests {
@@ -6,6 +7,25 @@ mod tests {
 
     #[test]
     fn it_works() {
-        println!("Started focused testing.")
+        let mut n1 = prelude::neuron::Neuron::new(
+            1,
+            5,
+            prelude::neuron::ActivationFunction::SIGMOID
+        );
+
+        let mut n2 = prelude::neuron::Neuron::new(
+            2,
+            7,
+            prelude::neuron::ActivationFunction::TANH
+        );
+
+        println!("Neuron 1: {:?}", n1);
+        println!("Neuron 2: {:?}", n2);
+
+        n1.shuffle(12039485u128, 1.0);
+        n2.shuffle(1039u128, 10.0);
+
+        println!("Neuron 1: {:?}", n1);
+        println!("Neuron 2: {:?}", n2);
     }
 }
