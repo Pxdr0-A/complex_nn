57c86dd947ce86a6b61f67efdf35cabece69f76c
Pedro Alves <pedromfa98@gmail.com>
2023-05-26 22:00:28 +0100

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -32,14 +32,14 @@ mod tests {
 
         n1.shuffle(457869485u128, &1.0);
         n2.shuffle(903923847u128, &1.0);
-        n3.shuffle(5598723423u128, &1.0);
+        n3.shuffle(559872342u128, &1.0);
         println!("Neuron 1: {:?}", n1);
         println!("Neuron 2: {:?}", n2);
         println!("Neuron 3: {:?}", n3);
 
         let output1 = n1.signal(&[0.1, 0.2]);
         let output2 = n2.signal(&[0.34, 1.977]);
-        let output3 = n3.signal(&[0.84, -0.577]);
+        let output3 = n3.signal(&[-0.84, 1.577]);
         println!("Neuron 1 output {:?}", output1);
         println!("Neuron 2 output {:?}", output2);
         println!("Neuron 3 output {:?}", output3);
