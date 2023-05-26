c0b78db5f1fbbb14f999db6a196f28f1d5bff81e
Pedro Alves <pedromfa98@gmail.com>
2023-05-26 17:19:14 +0100

    tested new methods

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,45 @@ pub mod math;
 
 #[cfg(test)]
 mod tests {
+    use std::f64::consts::PI;
+    use crate::math::Cf64;
     use super::*;
 
     #[test]
     fn it_works() {
         let mut n1 = prelude::neuron::Neuron::new(
             1,
-            5,
+            2,
             prelude::neuron::ActivationFunction::SIGMOID
         );
 
         let mut n2 = prelude::neuron::Neuron::new(
             2,
-            7,
+            2,
             prelude::neuron::ActivationFunction::TANH
         );
 
+        let mut n3 = prelude::neuron::Neuron::new(
+            3,
+            2,
+            prelude::neuron::ActivationFunction::RELU
+        );
         println!("Neuron 1: {:?}", n1);
         println!("Neuron 2: {:?}", n2);
+        println!("Neuron 3: {:?}", n3);
 
-        n1.shuffle(12039485u128, 1.0);
-        n2.shuffle(1039u128, 10.0);
-
+        n1.shuffle(457869485u128, &1.0);
+        n2.shuffle(903923847u128, &1.0);
+        n3.shuffle(5598723423u128, &1.0);
         println!("Neuron 1: {:?}", n1);
         println!("Neuron 2: {:?}", n2);
+        println!("Neuron 3: {:?}", n3);
+
+        let output1 = n1.signal(&[0.1, 0.2]);
+        let output2 = n2.signal(&[0.34, 1.977]);
+        let output3 = n3.signal(&[0.84, -0.577]);
+        println!("Neuron 1 output {:?}", output1);
+        println!("Neuron 2 output {:?}", output2);
+        println!("Neuron 3 output {:?}", output3);
     }
 }
