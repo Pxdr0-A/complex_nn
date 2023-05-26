2d0772fe36beb45000907b669a635f264d2ee152
Pedro Alves <pedromfa98@gmail.com>
2023-05-26 17:18:45 +0100

    corrected shuffle and developed signal method, created impl for ActivationFunction

--- src/prelude/neuron/mod.rs
+++ src/prelude/neuron/mod.rs
@@ -23,20 +23,72 @@ impl Neuron {
         Neuron { id, weights, activation }
     }
 
-    pub fn shuffle(&mut self, seed: u128, scale: f64) {
-        let mut next_val = seed;
+    pub fn shuffle(&mut self, mut seed: u128, scale: &f64) {
         let mut rnd_val1: f64 = 0.0;
         let mut rnd_val2: f64 = 0.0;
-        self.weights.iter_mut().map(|x| {
-            (next_val, rnd_val1) = random::lcg(next_val);
-            (next_val, rnd_val2) = random::lcg(next_val);
+        self.weights = self.weights.iter().map(|_| {
+            rnd_val1 = random::lcg(&mut seed);
+            rnd_val2 = random::lcg(&mut seed);
 
-            x.q = rnd_val1 * scale;
-            x.p = 2.0 * PI * rnd_val2 - PI;
-        }).collect::<Vec<_>>();
+            Cf64::new(
+                rnd_val1 * scale,
+                2.0 * PI * rnd_val2 - PI
+            )
+        }).collect();
     }
 
-    pub fn signal(&self, input: Vec<f64>) {
+    pub fn signal(&self, input: &[f64]) -> f64 {
+        // method 1 for feed forward
+        // check if input length is in agreement with neuron inputs
+        assert_eq!(self.weights.len(), input.len(),
+                   "Input length must match the number of neuron inputs."
+        );
+        let mut output = Cf64::new(0.0, 0.0);
+        for i in 0..input.len() {
+            // consider parallelism
+            output = Cf64::add(
+                &output,
+                &Cf64::mul(
+                    &self.weights[i],
+                    &Cf64::new(
+                          input[i],
+                          if input[i].is_sign_positive() { 0.0 } else { PI })
+            ));
+        }
 
+        println!("{:?}", output);
+
+        // build a macro out of this section
+        // hopefully to incorporate different approaches to decompose the complex output
+        self.activation.activate(&output)
+
+    }
+}
+
+impl ActivationFunction {
+    fn activate(&self, output: &Cf64) -> f64 {
+        match self {
+            ActivationFunction::SIGMOID => {
+                let active = |x: f64| -> f64 {
+                    1.0 / (1.0 + (-x).exp())
+                };
+
+                active(output.q * output.p.cos())
+            },
+            ActivationFunction::TANH => {
+                let active = |x: f64| -> f64 {
+                    x.tanh()
+                };
+
+                active(output.q * output.p.cos())
+            },
+            ActivationFunction::RELU => {
+                let active = |x: f64| -> f64 {
+                    if x.is_sign_positive() { x } else { 0.0 }
+                };
+
+                active(output.q * output.p.cos())
+            }
+        }
     }
 }
