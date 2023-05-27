d029f95bc16952fa7c173e68a9c9af3de601a60d
Pedro Alves <pedromfa98@gmail.com>
2023-05-27 21:51:57 +0100

    tested coherence of network generation

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,29 @@ pub mod math;
 mod tests {
     use std::f64::consts::PI;
     use crate::math::Cf64;
+    use crate::prelude::network::{ConventionalNetwork, Network};
+    use crate::prelude::neuron::ActivationFunction;
     use super::*;
 
     #[test]
     fn it_works() {
         let scale: f64 = 1.0;
-        let l = prelude::layer::Layer::new(
-            1,
+        let mut net: ConventionalNetwork = prelude::network::Network::new(
             2,
-            false,
-            prelude::neuron::ActivationFunction::SIGMOID,
-            3,
+            ActivationFunction::SIGMOID,
+            4,
+            38983,
             &scale
         );
 
-        println!("{:?}", l);
+        println!("{:?}", net);
+
+        net.add(
+            6,
+            ActivationFunction::TANH,
+            53898
+        );
+
+        println!("{:?}", net);
     }
 }
