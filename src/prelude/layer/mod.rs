ea1fe2d452fbf0d9bcd6b47de995a92903be2454
Pedro Alves <pedromfa98@gmail.com>
2023-05-27 09:56:10 +0100

    done for now with layer module

--- src/prelude/layer/mod.rs
+++ src/prelude/layer/mod.rs
@@ -1,7 +1,42 @@
-use super::neuron::Neuron;
+use crate::prelude::neuron::ActivationFunction;
+use crate::prelude::neuron::Neuron;
 
+#[derive(Debug)]
 pub struct Layer {
     id: usize,
     units: Vec<Neuron>,
-    hidden:bool
+    hidden: bool
+}
+
+impl Layer {
+    // to create the input layer
+    pub fn new(
+        id: usize,
+        n_units: usize,
+        hidden: bool,
+        activation: ActivationFunction,
+        inputs: usize,
+        scale: &f64) -> Layer {
+        // inputs on each neuron are considered in spite of input length
+        // error handling is done prior to this
+        let mut units: Vec<Neuron> = Vec::with_capacity(n_units);
+        let mut neuron;
+        let seed_increment: u128 = 999;
+        for u in 0..n_units {
+            neuron = Neuron::new(
+                u,
+                inputs,
+                // we need to clone because every neuron has its own activation
+                activation.clone()
+            );
+            neuron.shuffle(
+                (u as u128) + seed_increment,
+                scale
+            );
+
+            units.push(neuron);
+        }
+
+        Layer { id, units, hidden }
+    }
 }
