4e84c24a83c4f72ae280f67a708ec5e4a6d55b09
Pedro Alves <pedromfa98@gmail.com>
2023-05-27 09:57:00 +0100

    tested layer module

--- src/lib.rs
+++ src/lib.rs
@@ -9,39 +9,16 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut n1 = prelude::neuron::Neuron::new(
+        let scale: f64 = 1.0;
+        let l = prelude::layer::Layer::new(
             1,
             2,
-            prelude::neuron::ActivationFunction::SIGMOID
-        );
-
-        let mut n2 = prelude::neuron::Neuron::new(
-            2,
-            2,
-            prelude::neuron::ActivationFunction::TANH
-        );
-
-        let mut n3 = prelude::neuron::Neuron::new(
+            false,
+            prelude::neuron::ActivationFunction::SIGMOID,
             3,
-            2,
-            prelude::neuron::ActivationFunction::RELU
+            &scale
         );
-        println!("Neuron 1: {:?}", n1);
-        println!("Neuron 2: {:?}", n2);
-        println!("Neuron 3: {:?}", n3);
 
-        n1.shuffle(457869485u128, &1.0);
-        n2.shuffle(903923847u128, &1.0);
-        n3.shuffle(559872342u128, &1.0);
-        println!("Neuron 1: {:?}", n1);
-        println!("Neuron 2: {:?}", n2);
-        println!("Neuron 3: {:?}", n3);
-
-        let output1 = n1.signal(&[0.1, 0.2]);
-        let output2 = n2.signal(&[0.34, 1.977]);
-        let output3 = n3.signal(&[-0.84, 1.577]);
-        println!("Neuron 1 output {:?}", output1);
-        println!("Neuron 2 output {:?}", output2);
-        println!("Neuron 3 output {:?}", output3);
+        println!("{:?}", l);
     }
 }
