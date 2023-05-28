16799c3c1d7071f8dc107106c3e2b9bca9950e2e
Pedro Alves <pedromfa98@gmail.com>
2023-05-28 12:49:46 +0100

    tested matrix

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,27 @@ pub mod math;
 #[cfg(test)]
 mod tests {
     use std::f64::consts::PI;
-    use crate::math::Cf64;
+    use crate::math::{Cf64, Matrix};
     use crate::prelude::network::{ConventionalNetwork, Network};
     use crate::prelude::neuron::ActivationFunction;
     use super::*;
 
     #[test]
     fn it_works() {
-        let scale: f64 = 1.0;
-        let mut net: ConventionalNetwork = prelude::network::Network::new(
-            2,
-            ActivationFunction::SIGMOID,
-            4,
-            38983,
-            &scale
-        );
+        let mut m: Matrix<i32> = math::Matrix::new([3, 3]);
 
-        println!("{:?}", net);
+        m.add_row(&mut vec![1; 3]);
+        println!("{:?}", m);
+        m.add_row(&mut vec![4; 3]);
+        println!("{:?}", m);
 
-        net.add(
-            6,
-            ActivationFunction::TANH,
-            53898
-        );
+        let mut l: Matrix<f64> = math::Matrix::new([2, 2]);
 
-        println!("{:?}", net);
+        l.add_row(&mut vec![1.4, 2.34]);
+        println!("{:?}", l);
+        l.add_row(&mut vec![3.225, 4.21]);
+        println!("{:?}", l);
+
+        println!("{:?}", l.elm(&1, &0));
     }
 }
