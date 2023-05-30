9738dda51fa88da132511b9a6db2ea035c1ff68e
Pedro Alves <pedromfa98@gmail.com>
2023-05-30 23:14:51 +0100

    tested error handling and new operations

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,34 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut m: Matrix<i32> = math::Matrix::new([3, 3]);
+        let mut m: Matrix<i32> = Matrix::new([4, 4]);
 
         m.add_row(&mut vec![1; 3]);
         println!("{:?}", m);
         m.add_row(&mut vec![4; 3]);
         println!("{:?}", m);
+        m.add_row(&mut vec![5; 3]);
+        println!("{:?}", m);
 
-        let mut l: Matrix<f64> = math::Matrix::new([2, 2]);
+        println!("{:?}", m.elm(&1, &2));
+
+        let mut l: Matrix<f64> = Matrix::new([3, 3]);
 
         l.add_row(&mut vec![1.4, 2.34]);
         println!("{:?}", l);
-        l.add_row(&mut vec![3.225, 4.21]);
+        l.add_row(&mut vec![3.22, 4.21]);
         println!("{:?}", l);
 
         println!("{:?}", l.elm(&1, &0));
+
+        println!("{:?}", m.row(&2));
+        println!("{:?}", l.row(&1));
+        println!("{:?}", m.column(&0));
+        println!("{:?}", l.column(&0));
+
+        m.add_col(&mut vec![1, 3, 8]);
+        l.add_col(&mut vec![1.4, 6.45]);
+        println!("{:?}", m);
+        println!("{:?}", l);
     }
 }
