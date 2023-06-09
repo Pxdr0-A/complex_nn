e5c3ede4a65621deedac0f5b9d1000f919b06cae
Pedro Alves <pedromfa98@gmail.com>
2023-06-09 19:10:14 +0100

    done with exporting TabularDataset to csv format

--- src/prelude/dataset/mod.rs
+++ src/prelude/dataset/mod.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use std::collections::HashMap;
 
 use crate::math::Matrix;
@@ -24,6 +23,7 @@ Consider creating a trait Dataset.
 */
 
 impl<H, B, T> TabularDataset<H, B, T> {
+    
     pub fn new(capacity: [usize; 2]) -> TabularDataset<H, B, T> {
 
         let header: Vec<H> = Vec::with_capacity(capacity[1]);
@@ -41,15 +41,6 @@ impl<H, B, T> TabularDataset<H, B, T> {
         }
     }
 
-    pub fn from_csv(file: String, batch_size: usize) -> std::io::Result<()>  {
-
-        let file1 = fs::read_to_string("./resources/dataset.csv")?;
-
-        println!("{:?}", file1);
-
-        Ok(())
-    }
-
     pub fn add_row(&mut self, row: &mut Vec<B>, target_val: T) {
 
         self.body.add_row(row);
@@ -60,9 +51,18 @@ impl<H, B, T> TabularDataset<H, B, T> {
 
         (self.body.row(i), &self.target[*i])
     }
+
+    pub fn from_csv(file: &str, batch_size: usize)  {
+
+    }
 }
 
+// specific implementation for a synthetic clustering dataset
+// head - string
+// body - f64
+// target - f64
 impl TabularDataset<String, f64, f64> {
+    
     pub fn sample(
         shape: [usize; 2], 
         n_classes: usize, 
@@ -82,5 +82,42 @@ impl TabularDataset<String, f64, f64> {
         crate::utils::misc::add_random_points(&mut dataset, &centers, &shape, n_classes, seed);
 
         dataset
+    }
+
+    pub fn to_csv(&self, path: &str) {
+        
+        let mut contents: String = "".to_string();
+        
+        let mut formated_header: Vec<&str> = Vec::new();
+        for s in self.header.iter() {
+            formated_header.push(s.as_str());
+        }
+
+        contents.push_str(formated_header.join(",").as_str());
+        contents.push_str("\n");
+
+        let mut formated_bulk: Vec<String> = Vec::new();
+        let mut current_row: (&[f64], &f64);
+        for i in 0..self.body.shape[0] {
+            current_row = self.row(&i);
+            for f in current_row.0 {
+                // add body of the row to content Vec (values of the features)
+                formated_bulk.push(f.to_string());
+            }
+
+            // add target of the row to content Vec
+            formated_bulk.push(current_row.1.to_string());
+
+            // add auxiliar content Vec to contents to create a csv row
+            contents.push_str(formated_bulk.join(",").as_str());
+            contents.push_str("\n");
+
+            // clear auxiliar content Vec
+            formated_bulk.clear();
+        }
+
+        crate::utils::file_ops::write_csv(path, contents)
+            .expect("Failed to export TabularDataset to csv format.");
+
     }
 }
