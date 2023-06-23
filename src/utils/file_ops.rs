d2bbffa6396fd183895ade758f790c19b11ec9a4
Pedro Alves <pedromfa98@gmail.com>
2023-06-23 16:24:05 +0100

    added read file function

--- src/utils/file_ops.rs
+++ src/utils/file_ops.rs
@@ -7,9 +7,20 @@
  */
 
 use std::fs;
+use std::error::Error;
+
 
 pub fn write_csv(path: &str, contents: String) -> std::io::Result<()> {
+   
    fs::write(path, contents)?;
 
    Ok(())
+}
+
+
+pub fn read_csv(path: &str) -> Result<String, Box<dyn Error>> {
+
+   let contents = fs::read_to_string(path)?;
+
+   Ok(contents)
 }
